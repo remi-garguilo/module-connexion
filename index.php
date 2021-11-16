5c917cc9d3bfda85e765c1fa084f22640e9ce486
remig13 <remi.garguilo@epitech.eu>
2021-11-16 15:39:41 +0100

    html fini

---
+++ index.php
@@ -0,0 +1,42 @@
+<html >
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/index.css" />
+        <title>Acceuil</title>
+    </head>
+<body>
+<header>
+<div class="containerHeader">
+	<p class="lead">Welcome to my site</p>
+	<div class="header-bar">
+		<h1 class="logo">
+		    <img  src="css/img/icons.png" />
+		</h1>
+		<ul class="slider-menu">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="inscription.php">Inscription</a></li>
+		</ul>
+	</div>
+</div>
+</header>
+<main>
+    <div  class="container">
+        <div class = "containTop">
+            <div class="containCenterOfTop">
+            <div></div>
+            <div></div>
+            <div></div>
+            </div>
+        </div>
+        <div class = "containBot">
+            <div class="containCenterOfTop">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+</main>
+</footer>
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+<html >
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="css/inscription.css" />
+        <title>Inscription</title>
+    </head>
+<body>
+<header>
+</header>
+<main>
+<div class="login-page">
+  <div class="form">
+    <form action="./index.php" method="post" class="login-form">
+    <input type="text" id="login" name="login" placeholder="Login"/>
+      <input type="text" id="nom" name="nom" placeholder="Nom"/>
+      <input type="text" id="prenom" name="prenom" placeholder="Prenom"/>
+      <input type="password" id="prenom" name="prenom" placeholder="Password"/>
+      <button>login</button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
+  </div>
+</div>
+</form>
+</main>
+</footer>
+</body>
+</html>
